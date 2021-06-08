280529046f71cc6654259939a1f0d834b5f05351
HermanMusyoki <44892358+HermanMusyoki@users.noreply.github.com>
2021-06-08 12:47:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./styles.css";
+import CharacterDesc from '../components/CharacterDesc';
+import Navbar from '../components/Navbar';
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <CharacterDesc />
+    </>
+  )
+}
+
+export default App;
